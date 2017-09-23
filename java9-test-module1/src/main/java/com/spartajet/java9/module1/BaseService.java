ebb1b5790b172de4a02433acc614fec171a10c79
spartajet <spartajet.guo@gmail.com>
2017-09-23 22:16:36 +0800

    Initial commit

---
+++ java9-test-module1/src/main/java/com/spartajet/java9/module1/BaseService.java
@@ -0,0 +1,13 @@
+package com.spartajet.java9.module1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @description
+ * @create 2017-09-23 下午9:39
+ * @email spartajet.guo@gmail.com
+ */
+public class BaseService {
+    private Logger logger = LoggerFactory.getLogger(BaseService.class);
+}
---
+++ java9-test-module1/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @description
+ * @create 2017-09-23 下午9:39
+ * @email spartajet.guo@gmail.com
+ */
+module java9.test.module1 {
+    requires slf4j.api;
+}
